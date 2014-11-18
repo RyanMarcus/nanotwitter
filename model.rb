e063e5ae6ea962a24f46099648f7488d6944012b
Ryan Marcus <ryan@rmarcus.info>
2014-11-18 10:09:30 -0500

    Moved DB code into model

---
+++ model.rb
@@ -0,0 +1,104 @@
+require 'sequel'
+require 'sqlite3'
+
+DB = Sequel.sqlite("nano.db") # an in-memory database
+
+DB.create_table? :account do
+  primary_key :account_id
+  String :email
+  String :password
+end
+
+DB.create_table? :tweet do
+  primary_key :id
+  foreign_key :account_id, :account
+  String :text
+  DateTime :when
+end
+
+DB.create_table? :follow do
+  foreign_key :user, :account
+  foreign_key :follows, :account
+end
+
+module Model
+
+  def Model.all_tweets()
+    tweets = DB[:tweet]
+    tweets = tweets.join(:account, :account_id => :account_id)
+    tweets = tweets.reverse_order(:when)
+    return tweets.limit(100).all
+  end
+
+  def Model.tweets_by (user_id)
+    tweets = DB[:tweet]
+    tweets = tweets.join(:account, :account_id => :account_id)
+    tweets= tweets.filter(:account__account_id => user_id)
+    tweets = tweets.reverse_order(:when)
+    return tweets.limit(100).all
+  end
+
+  def Model.get_name_of (user_id)
+    accounts = DB[:account]
+    user = accounts.filter(:account_id => user_id)
+
+    if user.count() == 0
+      return false
+    end
+
+    user = user.first
+    return user[:email]
+  end
+
+
+  def Model.get_id_of (email)
+    accounts = DB[:account]
+    user = accounts.filter(:email => email)
+
+    if user.count() == 0
+      return false
+    end
+
+    user = user.first
+    return user[:account_id]
+  end
+
+
+  def Model.test_credentials (email, password)
+    accounts = DB[:account]
+    user = accounts.filter(:email => email)
+
+    if user.count() == 0
+      return false, "No user is registered with that email address"
+    end
+
+    user = user.first
+    pass_hash = BCrypt::Password.new(user[:password])
+
+    if not pass_hash == password 
+      return false, "Invalid password"
+    end
+
+    return true, "Login OK"
+
+  end
+
+
+  def Model.create_account (email, password)
+    if account_exists? email
+      return false, "An account with that email address already exists"
+    end
+
+    accounts = DB[:account]
+    pass_hash = BCrypt::Password.create(password)
+    
+    accounts.insert(:email => email, :password => pass_hash.to_s)
+    return true, "account created!"
+  end
+
+  def Model.account_exists? (email)
+    accounts = DB[:account]
+    return accounts.filter(:email => email).count() != 0
+  end
+
+end
